b6033fa8b9cbdfa422b5ffc406de7f8301606a89
Bablukmr <bablukmr800@gmail.com>
2023-09-13 23:35:09 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, View, TextInput, Button } from "react-native";
+import AppStyles from "./styles/AppStyles";
+import { useState } from "react";
+import InlineTextButton from "./components/inlineTextButton";
 
 export default function App() {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const bgImg = require("./assets/bgs.jpg");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground source={bgImg} style={AppStyles.container}>
+      <View style={AppStyles.bacgroundCover}>
+        <Text style={[AppStyles.lightText, AppStyles.header]}>LogIn</Text>
+        <TextInput
+          style={[
+            AppStyles.lightText,
+            AppStyles.textInput,
+            AppStyles.lightTextInput,
+          ]}
+          placeholderTextColor="#fff"
+          placeholder="Username"
+          value={userName}
+          onChangeText={setUserName}
+        />
+        <TextInput
+          style={[
+            AppStyles.lightText,
+            AppStyles.textInput,
+            AppStyles.lightTextInput,
+          ]}
+          placeholderTextColor="#fff"
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <View style={AppStyles.rowContainer}>
+        <Text style={[AppStyles.lightText]}>Don't have an account? </Text>
+        <InlineTextButton text='Sign Up'/>
+        </View>
+        <View style={AppStyles.rowContainer}>
+        <Text style={[AppStyles.lightText]}>Forgotten your password? </Text>
+        <InlineTextButton text='Reset'/>
+        </View>
+        <Button title="LogIN"  />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/AppStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bacgroundCover: {
+    alignItems: "center",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    backgroundColor: "#908599",
+    padding: 16,
+    opacity: 0.6,
+  },
+  lightText: {
+    color: "#fff",
+  },
+  header: {
+    fontSize: 20,
+  },
+  textInput: {
+    alignSelf: "stretch",
+    padding: 8,
+    borderBottomWidth: 2,
+    marginVertical: 8,
+  },
+  lightTextInput: {
+    borderBottomColor: "#ffffff",
+  },
+  pressedInlineButton: {
+    color: "#4238ab",
+    opacity: 0.4,
+  },
+  inlineTextButtons: {
+    color: "#4238ab",
+  },
+  rowContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:"center",
+    marginVertical:4
+  }
+});
