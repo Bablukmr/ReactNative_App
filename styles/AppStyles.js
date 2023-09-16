b51d4f24cac5dc1bf5c7b6d43a87e098e3604404
Bablukmr <bablukmr800@gmail.com>
2023-09-16 20:08:29 +0530

    firebase/auth

--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -4,7 +4,7 @@ export default styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
+    alignItems: "center",
     justifyContent: "center",
   },
   bacgroundCover: {
